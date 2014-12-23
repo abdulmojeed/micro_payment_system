9a5310a3516d4f0622a18fa393bdf2ba9e038286
abdulmojeed <olurode_abdul@yahoo.com>
2014-12-23 16:59:38 +0100

    Single SQL file

--- create_dbs.sql
+++ create_dbs.sql
@@ -19,3 +19,27 @@ CREATE TABLE tb_students(id INT(25) NOT NULL AUTO_INCREMENT,
 						is_active VARCHAR(2),
 						PRIMARY KEY (enrollment_no, id)
 						);
+CREATE TABLE tb_termregistration(id INT(25) NOT NULL AUTO_INCREMENT,
+						today_date DATE NOT NULL,
+						enrollment_no VARCHAR(25) NOT NULL, 
+						class VARCHAR(25) NOT NULL,
+						term VARCHAR(25) NOT NULL,
+						year INT(25) NOT NULL,
+						amount VARCHAR(25) NOT NULL,
+						PRIMARY KEY (id)
+						);
+CREATE TABLE tb_payment(id INT(25) NOT NULL AUTO_INCREMENT,
+						payment_date DATE NOT NULL,
+						enrollment_no VARCHAR(25) NOT NULL, 
+						class VARCHAR(25) NOT NULL,
+						payment INT(25) NOT NULL,
+						pay_mode VARCHAR(25) NOT NULL,
+						receipt_no INT(25) NOT NULL,
+						teller_no INT(25),
+						PRIMARY KEY (id)
+						);
+CREATE TABLE tb_fees(id INT(25) NOT NULL AUTO_INCREMENT,
+						class VARCHAR(25) NOT NULL,
+						fees INT(25) NOT NULL, 
+						PRIMARY KEY (id)
+						);
